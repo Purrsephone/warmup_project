831ffa759c9cbeef6677d5df63c34176de0418ab
Purrsephone <askmeowtech@gmail.com>
2021-04-11 13:41:37 -0500

    pls just do as you're told

--- scripts/wall-follower.py
+++ scripts/wall-follower.py
@@ -14,7 +14,17 @@ dist_270 = 0
 dist_0 = 0
 start = True 
 turn_45 = False
-
+true_front = 0 
+turn_time = False 
+true_back = 0
+true_left = 0 
+true_right = 0 
+going_straight = False 
+curr_angle = 0 
+des_angle = ((90 * 2 * math.pi)/360.0)
+t1 = 0 
+t0 = 0 
+correction = False 
 class WallFollower(object):
 	def __init__(self):
 		rospy.init_node("follow_wall")
@@ -25,7 +35,79 @@ class WallFollower(object):
 		self.twist = Twist(linear=lin,angular=ang)
 	def process_scan(self, data):
 		global start 
+		global turn_45 
+		global curr_angle 
+		global t1 
+		global t0 
+		global correction
+		right_min = min(data.ranges[265:273])
+		right_max = max(data.ranges[265:273])
+		right = ((right_min + right_max) / 2.0)
+		right_bot_min =  min(data.ranges[220:228])
+		right_bot_max =  max(data.ranges[220:228])
+		right_bot = ((right_bot_min + right_bot_max) / 2.0)
+		right_top_min =  min(data.ranges[310:318])
+		right_top_max =  max(data.ranges[310:318])
+		right_top = ((right_top_min + right_top_max) / 2.0)
+		front_min = min(data.ranges[0:5] + data.ranges[-4:])
+		front_max = max(data.ranges[0:5] + data.ranges[-4:])
+		front = ((front_min + front_max) / 2.0)
+		if(start == True):
+			curr_angle = 0 
+			print("start")
+		#fix vertical 
+			if(abs(front - 0.8) <= 0.08):
+				self.twist.linear.x = 0
+				self.twist_pub.publish(self.twist)
+				start = False 
+				turn_45 = True 
+				t0 = rospy.Time.now().to_sec()
+				#true_front = front 
+				#true_back = back 
+				#true_left = left 
+				#true_right = right 
+			else: 
+				err = abs(0.5 - front)
+				k_p = 0.1
+				self.twist.linear.x = k_p * err
+				self.twist_pub.publish(self.twist)
+		if turn_45 == True:
+			print("turn") 
+			if(curr_angle < des_angle):
+				self.twist.angular.z = 0.05
+				self.twist_pub.publish(self.twist)
+				t1 = rospy.Time.now().to_sec()
+				curr_angle = 0.05 * (t1 - t0)
+			else: 
+				turn_45 = False 
+				correction = True 
+				self.twist.angular.z = 0.0
+				self.twist_pub.publish(self.twist)
+		if(correction == True): 
+			if(right_top < right):
+				self.twist.angular.z = -0.05
+				self.twist_pub.publish(self.twist)
+				print("top")
+			if(right_bot < right):
+				self.twist.angular.z = 0.05
+				self.twist_pub.publish(self.twist) 
+				#need to tilt 
+				print("bot")
+			else:
+				correction = False 
+				start = True 
+			
+	
+	
+		'''
+		global start 
 		global turn_45
+		global true_front
+		global true_back 
+		global true_left 
+		global true_right
+		global turn_time
+		global going_straight 
 		front_min = min(data.ranges[0:5] + data.ranges[-4:])
 		front_max = max(data.ranges[0:5] + data.ranges[-4:])
 		front = ((front_min + front_max) / 2.0)
@@ -38,17 +120,142 @@ class WallFollower(object):
 		right_min = min(data.ranges[265:273])
 		right_max = max(data.ranges[265:273])
 		right = ((right_min + right_max) / 2.0)
+		right_bot_min =  min(data.ranges[220:228])
+		right_bot_max =  max(data.ranges[220:228])
+		right_bot = ((right_bot_min + right_bot_max) / 2.0)
+		right_top_min =  min(data.ranges[310:318])
+		right_top_max =  max(data.ranges[310:318])
+		right_top = ((right_top_min + right_top_max) / 2.0)
+		
 		if(start == True):
-			if(abs(front - 0.5) <= 0.05):
+			#print(abs(front - 1.0)) 
+			#print("HERE")
+			if(abs(front - 1.0) <= 0.1):
 				self.twist.linear.x = 0
 				self.twist_pub.publish(self.twist)
 				start = False 
 				turn_45 = True 
-			err = abs(0.5 - data.ranges[0])
-			k_p = 0.1
-			self.twist.linear.x = k_p * err
+				true_front = front 
+				true_back = back 
+				true_left = left 
+				true_right = right 
+			else: 
+				err = abs(0.5 - front)
+				k_p = 0.1
+				self.twist.linear.x = k_p * err
+				self.twist_pub.publish(self.twist)
+		if(turn_45 == True):
+			print(true_front)
+			print(true_back)
+			print(true_left)
+			print(true_right) 
+			cond1 = abs(true_left - front) <= 0.1
+			cond2 = abs(true_right - back) <= 0.1
+			cond3 = abs(true_back - left) <= 0.1
+			cond4 = (right - true_front <= 0.05)
+			if(true_left == "inf"):
+				print("NFINITY MOTHFUCKA") 
+			if(cond4): 
+			#if(cond1 and cond2 and cond3 and cond4):
+				self.twist.angular.z = 0
+				self.twist_pub.publish(self.twist)
+				turn_45 = False
+				#start = True
+				going_straight = True 
+			else:
+				self.twist.angular.z = 0.05
+				self.twist_pub.publish(self.twist)
+			#print(front)
+			#print("\n")
+			#print(data.ranges[179])
+		if(going_straight == True):
+			self.twist.linear.x = 0.1
 			self.twist_pub.publish(self.twist)
-			#print(data.ranges[0])
+			if((right - 1.0) > 0.05):
+				self.twist.angular.z = 0.05
+				self.twist_pub.publish(self.twist)
+			if((right - 1.0) < -0.05):
+				self.twist.angular.z = -0.05
+				self.twist_pub.publish(self.twist)
+		'''
+			
+		'''
+
+		elif(turn_45 == True):
+				print("along wall")
+				print(data.ranges[269])
+				if(data.ranges[0] <= 0.5):
+					self.twist.linear.x = 0
+					self.twist_pub.publish(self.twist)
+					dist_270 = data.ranges[269]
+					dist_0 = data.ranges[0]
+					turn_time = True 
+					turn_45 = False 
+					print("TURN TIME")
+				elif(data.ranges[269] == 0.5):
+					self.twist.angular.z = 0
+					self.twist.linear.x = 0.2
+					self.twist_pub.publish(self.twist)
+				elif(data.ranges[269] < 0.5):
+					print("less than 0.5")
+					self.twist.linear.x = 0.2
+					self.twist.angular.z = 0.03
+					self.twist_pub.publish(self.twist)
+				elif(data.ranges[269] > 0.5):
+					print("greater than 0.5") 
+					self.twist.linear.x = 0.2
+					self.twist.angular.z = -0.03
+					self.twist_pub.publish(self.twist)
+				else:
+					prini("BAD")
+				#else: 
+					#self.twist.angular.z = 0
+					#self.twist.linear.x = 0.2
+					#self.twist_pub.publish(self.twist)
+		if(turn_time == True):
+			#done = ((data.ranges[179] <= 0.5) and (data.ranges[269] <= 0.5))
+			#done = abs(data.ranges[269] 
+			done = (abs(data.ranges[179] - dist_270) <= 0.1 and (abs(data.ranges[269] - dist_0) <= 0.1) and (data.ranges[0] > 2.5) and (data.ranges[89] > 2.5))
+			if(done == True):
+				self.twist.angular.z = 0
+				self.twist_pub.publish(self.twist)
+				turn_time = False
+				turn_45 = True 
+				print("DONE TURNING") 
+			else:
+				self.twist.angular.z = 0.1
+				self.twist_pub.publish(self.twist)
+				'''
+				
+		'''
+		if(turn_45 == True):
+			min_dist = (min(data.ranges))
+			print("min dist:") 
+			print(min_dist) 
+			print(right - min_dist)
+			print(abs(right_top - (min_dist * math.sqrt(2))))
+			cond1 = (right - min_dist) <= 0.08
+			cond2 = abs(right_bot - (min_dist * math.sqrt(2))) <= 0.08
+			cond3 = abs(right_top - (min_dist * math.sqrt(2))) <= 0.08
+			#if(not cond1):
+				#print(cond1)
+			#if(not cond2):
+				#print(cond2)
+			#if(not cond3):
+			if(cond3):
+				print(cond3)	
+			if(cond1):
+				self.twist.angular.z = 0.0
+				self.twist.linear.x = 0.1
+				self.twist_pub.publish(self.twist)
+				print("forward") 
+				#tiime to go forward
+				turn_45 = False
+				start = True 
+			else:
+				self.twist.angular.z = 0.03
+				self.twist_pub.publish(self.twist)
+		'''
 		'''
 		if(turn_45 == True):
 			cond1 = abs(right - 0.5) <= 0.01
